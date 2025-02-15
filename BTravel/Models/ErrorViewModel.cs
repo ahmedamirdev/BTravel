551f74140d8ced7e80f76230259376745cd037fb
Ahmed Amir <ahmed.amir@grandmstg.com>
2025-02-15 13:42:01 +0200

    Add project files.

---
+++ BTravel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BTravel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
