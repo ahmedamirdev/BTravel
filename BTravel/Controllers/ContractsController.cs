6cfeb88df9f1159e3ca215f9a5dd28c1e2c5fc68
MostafaSafey <58831144+safeyz77@users.noreply.github.com>
2025-02-17 20:36:04 +0200

    Add Dashboard Tamplate to the Project ( with no icon)

---
+++ BTravel/Controllers/ContractsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTravel.Controllers
+{
+    public class ContractsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Dashboard/Contracts/Index.cshtml");
+        }
+    }
+}
