326e9e41a18ba8b0192804b935fbc75c289646b9
MostafaSafey <58831144+safeyz77@users.noreply.github.com>
2025-02-22 17:58:22 +0200

    Done first base

---
+++ BTravel/Controllers/ContractController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTravel.Controllers
+{
+    public class ContractController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View("~/Views/Shared/_LayoutDashboard.cshtml");
+        }
+    }
+}
--- BTravel/Controllers/HomeController.cs
+++ BTravel/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
-using BTravel.Models;
+using BTravel.DAL.Entities;
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace BTravel.Controllers
@@ -15,19 +16,41 @@ namespace BTravel.Controllers
 
         public IActionResult Index()
         {
-            return View();
-        } 
+            List<Contract> data = new List<Contract>();
+
+            data.Add(new Contract
+            {
+                ContractId = 1,
+                HotelName = "shiraton",
+
+            });
+            data.Add(new Contract
+            {
+                ContractId = 2,
+                HotelName = "sofitel",
+
+            });
+            data.Add(new Contract
+            {
+                ContractId = 3,
+                HotelName = "helton",
+
+            });
+
+
+            return View(data);
+        }
         public IActionResult Test()
         {
             return View();
         }
 
-     
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
     }
 }
